a89aa4efed581015d9961610ce9a1b2dcef2c650
Inori Sakuma <lizzy@lainzlab.com>
2014-03-21 07:08:54 +0900

    修正コミット

---
+++ mozakin_crawler/src/jp/co/lizzy/common/http/HttpException.java
@@ -0,0 +1,29 @@
+package jp.co.lizzy.common.http;
+
+public class HttpException extends Exception {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 911196516095480947L;
+
+	public HttpException() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public HttpException(String message) {
+		super(message);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public HttpException(Throwable cause) {
+		super(cause);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public HttpException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+}
